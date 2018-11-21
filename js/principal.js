87bd6557bc728edf48deec8526ec232486297d46
Dyogo Viana <dyogo.vianaa@gmail.com>
2018-11-21 00:47:15 -0200

    JS | IMC no console.log.

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,20 @@
 // mudar título do h1.
-
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+
+// IMC = peso / altura * altura
+var paciente = document.querySelector ("#primeiro-paciente");
+
+// Peso
+var tdPeso = paciente.querySelector (".info-peso");
+var peso = tdPeso.textContent;
+
+// Altura
+var tdAltura = paciente.querySelector (".info-altura");
+var altura =  tdAltura.textContent;
+
+// IMC
+var imc = peso / (altura * altura); // calculo, 100 / 2 * 2 = 25
+
+console.log (imc);
