7b6a9055d001fde87ec24f147007debe406a9be8
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-01-17 15:53:24 -0200

    Adicionado o paciente na tabela.

--- js/form.js
+++ js/form.js
@@ -17,8 +17,41 @@ var botaoAdicionar = document.querySelector ("#adicionar-paciente");
 
 botaoAdicionar.addEventListener ("click", function (event) {
     event.preventDefault ();
-    console.log ("Oi, eu sou o botão e fui clicado.")
+    
+    var form = document.querySelector ("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement ("tr"); 
+
+    var nomeTd = document.createElement ("td");
+    var pesoTd = document.createElement ("td");
+    var alturaTd = document.createElement ("td");
+    var gorduraTd = document.createElement ("td");
+    var imcTd = document.createElement ("td");
+    
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    // Adicionar <td> nas <tr>.
+    pacienteTr.appendChild (nomeTd);
+    pacienteTr.appendChild (pesoTd);
+    pacienteTr.appendChild (alturaTd);
+    pacienteTr.appendChild (gorduraTd);
+
+    // Adicionar o paciente dentro da tabela.
+    var tabela = document.querySelector ("#tabela-pacientes");
+    tabela.appendChild (pacienteTr);
 })
+
+
+
+
 // Acinama tem uma função anônima (sem parâmetro). Ela funciona igual ao exemplo com a função nominada acima.
 
 // console.log (botaoAdicionar); ----> assim pode ser testado e ver se tá realmente chamando o btn.
