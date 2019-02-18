648b6b2b208eae1bfd44c629bfd806f24710cd34
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-02-18 17:09:16 -0300

    JS & CSS | Animando a remoção do paciente.

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -10,5 +10,9 @@
 var tabela = document.querySelector ("table");
 
 tabela.addEventListener ("dblclick", function (event) {
-    event.target.parentNode.remove ();
+    event.target.parentNode.classList.add ("fadeOut");
+
+    setTimeout (function () {
+        event.target.parentNode.remove ();
+    }, 500); // 0.5 segundos.
 });
