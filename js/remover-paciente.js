6db99b42759224153887004e216a038f40a8504e
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-02-18 16:20:23 -0300

    JS | Delegando eventos; removendo pacientes; 'parentNode';

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,11 +1,14 @@
 // Remover pacientes.
 
+
+
 // Selceciona todas as linhas com a classe 'paciente'. 'dblclick' = duplo clique.
 // 'this.remove', é para retirar o dono do evendo, no caso o 'paciente'.
-var pacientes = document.querySelectorAll (".paciente");
+// event (click); target (o que foi clicado);
+// Para selecionarmos o pai de um elemento, trabalharemos com a propriedade 'parentNode'.
 
-pacientes.forEach (function (paciente) {
-    paciente.addEventListener ("dblclick", function () {
-        this.remove ();
-    })
-})
+var tabela = document.querySelector ("table");
+
+tabela.addEventListener ("dblclick", function (event) {
+    event.target.parentNode.remove ();
+});
